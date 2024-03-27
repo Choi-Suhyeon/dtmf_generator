325af6945b10637e7b5b7baee75102959ca260ea
SUHYEON CHOI <1AzyF0x@protonmail.ch>
2024-03-27 14:23:22 +0900

    add sound output functionality for the entered dials

--- README.md
+++ README.md
@@ -3,6 +3,17 @@
 This is a script developed as an assignment for the Wireless Mobile Systems and Security class, generating DTMF (Dual Tone Multi-Frequency) signals.
  무선이동시스템및보안 수업에서 과제로 진행한 DTMF(Dual Tone Multi-Frequency) 신호를 발생시키는 스크립트입니다.
 
-## TO-DO
+## USAGE GUIDELINES
+입력 필드에 휴대 전화 다이얼 번호(0~9, *, #)를 입력한 후, 아래 Write 버튼을 누르거나 키보드에서 Enter를 누르면 아래와 같은 정보를 확인할 수 있습니다.
+- 시간에 따른 진폭 그래프: 현재는 여러 번호를 입력한 경우 그래프들이 겹쳐져 보이게 됩니다.
+- 이에 대한 주파수 그래프 : 이것 또한 마찬가지로 그래프들이 겹쳐져 보이게 됩니다. 다만, 중복하여 출력되는 그래프는 없습니다.
+- 다이얼 소리 : 입력한 다이얼 번호의 소리를 출력합니다.
+
+예시 : 123456789*# ENTER
+
+프로그램 하단 상태바를 통해 입력에 유효하지 않은 번호가 포함되어 있었는지 여부를 확인할 수 있습니다.
+
+## TO-DO LIST
 - ~~0~9, *, # 중 하나가 주어졌을 때, 주어진 숫자(또는 문자)에 해당하는 DTMF 신호 그래프를 그리고 소리 출력.~~
 - ~~가능하면, GUI로 깔끔하게 프로그램 만들기.~~
+- 여러 입력이 주어졌을 때 시간 축을 기준으로 하는 파형 그래프는 출력되는 소리와 매칭되게 출력시키기.
--- main.py
+++ main.py
@@ -1,22 +1,29 @@
 from PyQt5.QtWidgets import QApplication, QDesktopWidget, QMainWindow, QVBoxLayout, QWidget, QPushButton, QLineEdit
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 from matplotlib.figure import Figure
-from PyQt5.QtCore import Qt
+from scipy.io.wavfile import write
+from datetime import datetime
+from itertools import repeat
 
 import numpy as np
+import subprocess
 import sys
+import os
 
 
 class DtmfAnalyzer(QMainWindow):
     __sampling_frequency = 100000
     __duration           = 0.5
     __time_array         = np.linspace(0, __duration, int(__sampling_frequency * __duration))
+    __interval_sound     = np.fromiter(repeat(np.int16(0), int(__sampling_frequency * __duration / 4)), dtype=np.int16)
     __dtmf_frequencies   = {
         '1': (697, 1209), '2': (697, 1336), '3': (697, 1477),
         '4': (770, 1209), '5': (770, 1336), '6': (770, 1477),
         '7': (852, 1209), '8': (852, 1336), '9': (852, 1477),
         '*': (941, 1209), '0': (941, 1336), '#': (941, 1477),
     }
+
+    __dtmf_signals = dict()
 
     def __init__(self):
         super().__init__()
@@ -38,11 +45,12 @@ class DtmfAnalyzer(QMainWindow):
         self.__layout.addWidget(self.__input_button)
         self.__layout.addWidget(self.__plot_canvas)
 
+        self.__input_field.setPlaceholderText('Enter one or more phone dial numbers')
         self.__input_field.returnPressed.connect(self.__process_input)
         self.__input_button.clicked.connect(self.__process_input)
 
-        self.ax1 = self.__plot_canvas.figure.add_subplot(211)
-        self.ax2 = self.__plot_canvas.figure.add_subplot(212)
+        self.__ax1 = self.__plot_canvas.figure.add_subplot(211)
+        self.__ax2 = self.__plot_canvas.figure.add_subplot(212)
 
         self.__set_graph1()
         self.__set_graph2()
@@ -62,20 +70,20 @@ class DtmfAnalyzer(QMainWindow):
         self.move(window_frame.topLeft())
 
     def __set_graph1(self):
-        self.ax1.clear()
-        self.ax1.set_title('Time-Domain Waveform')
-        self.ax1.set_xlabel('Time (s)')
-        self.ax1.set_ylabel('Amplitude')
-        self.ax1.set_ylim((-2.25, 2.25))
-        self.ax1.grid(True)
+        self.__ax1.clear()
+        self.__ax1.set_title('Time-Domain Waveform')
+        self.__ax1.set_xlabel('Time (s)')
+        self.__ax1.set_ylabel('Amplitude')
+        self.__ax1.set_ylim((-2.25, 2.25))
+        self.__ax1.grid(True)
 
     def __set_graph2(self):
-        self.ax2.clear()
-        self.ax2.set_title('Frequency-Domain Waveform')
-        self.ax2.set_xlabel('Frequency (Hz)')
-        self.ax2.set_ylabel('Magnitude')
-        self.ax2.set_xlim((0, 2000))
-        self.ax2.grid(True)
+        self.__ax2.clear()
+        self.__ax2.set_title('Frequency-Domain Waveform')
+        self.__ax2.set_xlabel('Frequency (Hz)')
+        self.__ax2.set_ylabel('Magnitude')
+        self.__ax2.set_xlim((0, 2000))
+        self.__ax2.grid(True)
 
     def __process_input(self):
         text = self.__input_field.text().strip()
@@ -87,14 +95,24 @@ class DtmfAnalyzer(QMainWindow):
 
             return
 
+        self.__dtmf_signals.clear()
         self.__set_graph1()
         self.__set_graph2()
 
         for c in sorted(set(text)):
             self.__draw_plots(c)
 
+        scaled_concat_wave = np.concatenate([np.int16(0x7FFF * self.__dtmf_signals[c]) if c != '$' else self.__interval_sound for c in '$'.join(text)])
+        name_elem_datetime = datetime.now().strftime('%Y%m%d_%H%M%S')
+        name_elem_nums     = ''.join(text).translate(str.maketrans('*#', 'as'))
+        audio_file_name    = f'{name_elem_datetime}_{name_elem_nums}.wav'
+        audio_file_path    = os.path.join(os.getcwd(), audio_file_name)
+
+        write(audio_file_name, self.__sampling_frequency, scaled_concat_wave)
+        subprocess.run(['start', audio_file_path], shell=True)
+
         self.statusBar().showMessage('Success: graphs corresponding to the input has been plotted successfully')
-        self.ax2.legend(loc='best', frameon=True)
+        self.__ax2.legend(loc='best', frameon=True)
         self.__plot_canvas.figure.tight_layout()
         self.__plot_canvas.draw()
 
@@ -105,12 +123,14 @@ class DtmfAnalyzer(QMainWindow):
         half_frequencies = frequencies[:len(frequencies) >> 1]
         half_dtmf_fft    = dtmf_fft[:len(frequencies) >> 1]
 
-        self.ax1.plot(self.__time_array, dtmf_signal)
-        self.ax2.plot(half_frequencies, np.abs(half_dtmf_fft), label=dial_number)
+        self.__dtmf_signals[dial_number] = dtmf_signal
+
+        self.__ax1.plot(self.__time_array, dtmf_signal)
+        self.__ax2.plot(half_frequencies, np.abs(half_dtmf_fft), label=dial_number)
 
     def __generate_dtmf_signal(self, dial_num):
         freq1, freq2 = self.__dtmf_frequencies.get(dial_num, (0, 0))
-        signal = self.__get_sin(freq1, time=self.__time_array) + self.__get_sin(freq2, time=self.__time_array)
+        signal       = self.__get_sin(freq1, time=self.__time_array) + self.__get_sin(freq2, time=self.__time_array)
 
         return signal
 
